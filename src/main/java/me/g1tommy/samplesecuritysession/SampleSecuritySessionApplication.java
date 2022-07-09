5f30003abf4d830993e971fe84dc3854cb9560d6
G1 <g1.tommy.jeon@gmail.com>
2022-07-09 17:59:38 +0900

    feat: Initial Commit

---
+++ src/main/java/me/g1tommy/samplesecuritysession/SampleSecuritySessionApplication.java
@@ -0,0 +1,13 @@
+package me.g1tommy.samplesecuritysession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSecuritySessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSecuritySessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/g1tommy/samplesecuritysession/SampleSecuritySessionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.g1tommy.samplesecuritysession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSecuritySessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
